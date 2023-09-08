0153ae114aa34cdc6c5e5f7ab5acbe4fae476957
pedrovidal <pedrovidal@escavador.com>
2023-09-08 14:57:47 -0300

    Parse rtf files with striprtf

--- textract/parsers/rtf_parser.py
+++ textract/parsers/rtf_parser.py
@@ -1,15 +1,12 @@
-import six
+from striprtf.striprtf import rtf_to_text
 
-from .utils import ShellParser
+from .utils import BaseParser
 
 
-class Parser(ShellParser):
-    """Extract text from rtf files using unrtf.
-    """
+class Parser(BaseParser):
+    """Extract text from rtf files using striprtf."""
 
     def extract(self, filename, **kwargs):
-        # http://superuser.com/a/243089/126633
-        stdout, stderr = self.run(['unrtf', '--text', filename])
-        splitter = six.b('-') * 17 + six.b('\n')
-        text_conversion = stdout.split(splitter, 1)[-1]
-        return text_conversion
+        with open(filename) as stream:
+            rtf = stream.read()
+        return rtf_to_text(rtf)
